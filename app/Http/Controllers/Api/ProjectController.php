bb0767e70c8b010f3d478aaf99b8af1b1e8e35f0
Tagliapietra96 <tagliatek96@gmail.com>
2023-02-15 15:47:41 +0100

    add pagination

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,7 +10,8 @@ class ProjectController extends Controller
 {
   
 public function index() {
-	$projects = Project::all();
+	// $projects = Project::all();
+	$projects = Project::paginate();
 
 	return response()->json($projects);
 }
